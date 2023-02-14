23792f13448c671877a1ef4c065a8628c43debe3
Andy Hershberger <andyhersh43@gmail.com>
2023-02-13 22:34:35 -0700

    ui for pause button

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Main simulation loop
 
-import pygame
+import pygame, pygame_gui
 from lib.creatures import Plant, Herbivore, Carnivore
 from lib.world import World
 import sys
@@ -9,19 +9,27 @@ import time
 if __name__ == '__main__':
     pygame.init()
 
-    WIDTH = 800
-    HEIGHT = 800
+    SIM_WIDTH = 1500
+    SIM_HEIGHT = 1000
 
-    SCREEN_SIZE = (WIDTH, HEIGHT)
+    #TODO implement these somehow
+    #GUI_WIDTH = 1920
+    #GUI_HEIGHT = 1080
+
+    SIM_SIZE = (SIM_WIDTH, SIM_HEIGHT)
+    sim_screen = pygame.display.set_mode(SIM_SIZE, 0, 32)
+
+    #GUI_SIZE = (GUI_WIDTH, GUI_HEIGHT)
+    #gui_man = pygame_gui.UIManager(GUI_SIZE)
+    gui_man = pygame_gui.UIManager(SIM_SIZE)
+    pause_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(1425, 925, 50, 50), text='||', manager=gui_man)
 
     clock = pygame.time.Clock()
-    screen = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
-    pixels_per_sec = 10
+    is_running = True
 
-    world = World(SCREEN_SIZE)
+    world = World(SIM_SIZE)
 
-    gen_h = True
-    gen_c = True
+    sex_setter = True
 
     #Add Creatures
     for i in range(50):
@@ -29,24 +37,31 @@ if __name__ == '__main__':
         time.sleep(0.01)
     
     for i in range(15):
-        world.add_creature(Herbivore(world, sex=gen_h))
+        world.add_creature(Herbivore(world, sex=sex_setter))
         time.sleep(0.01)
-        world.add_creature(Carnivore(world, sex=gen_c))
+        world.add_creature(Carnivore(world, sex=sex_setter))
         time.sleep(0.01)
-        gen_c = not gen_c
-        gen_h = not gen_h
+        sex_setter = not sex_setter
 
-    while 1:
+    paused = False
+    while is_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                sys.exit()
+                is_running = False
+            elif event.type == pygame.USEREVENT and event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == pause_button:
+                    paused = not paused
+            gui_man.process_events(event)
         
         #Sim Loop
-        screen.fill((0,150,20))
-        time_elapsed = clock.tick(70)
+        sim_screen.fill((0,150,20))
+        time_elapsed = clock.tick(60)
         time_elapsed = time_elapsed/1000
+        gui_man.update(time_elapsed)
 
         world.process(time_elapsed)
-        world.render(screen)
+        world.render(sim_screen)
+        #gui_man.draw_ui(GUI_SIZE)
+        gui_man.draw_ui(sim_screen)
 
         pygame.display.update()
